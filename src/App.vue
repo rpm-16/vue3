94e21270f4ed540949d03be77746d162b5cfd7dd
rpm-16 <rpm-16@mail.ru>
2022-12-07 00:14:56 +0300

    Фундаментальный курс Vue3 19:29 https://www.youtube.com/watch?v=XzLuMtDelGk&t=490s

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>  Counted {{likes}}</div>
+    <button v-on:click="like">Like me</button>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                likes: 0,
+            }
+        },
+        methods: {
+            like(){ 
+                this.likes += 1;
+            }
+        }
+    }
+</script>
+
+<style>
+
+</style>
